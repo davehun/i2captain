92e2e1e50b9f8d194e31bf709ae9366b9b10e0f2
David Hunnisett <david@inferiorplanet.co.uk>
2012-07-28 11:05:30 +0100

    updated readme and added working examples

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-i2captain
-=========
+# i2captain #
 
-Nmea network multiplexor 
+
+NMEA network multiplexor.
+There are 2 programs i2Captain and i2armChairCaptain.
+They allow NMEA data to be sent to multiple ios, andriod and PCs for
+logging / display.
+They can be used either live (i2Captain) or after the event
+(i2armChairCaptain).
+
+
+## i2Captain ##
+
+Copies data from a serial port and broadcasts as UDP (port 51243) and a muti
+threaded TCP server (port 2114).
+
+### Usage ###
+> python i2Captain.py > log
+This will run the application and capture the log into a file called
+log suitable for use by i2armChairCaptain
+### Todo ###
+* Exit cleanly from control-c
+* Log into file with timestamps
+* Compress log files.
+* Log rotation
+
+## i2armChairCaptain ##
+Takes a rate and logfile as argument.
+Replays the log file NMEA over the same TCP and UDP ports as
+i2Captain.
+### Usage ###
+> python i2armChairCaptain.py examples/holheadrace
+this will start the simulator using some example data collected during
+a race from HolyHead.
+
+### Todo ###
+* Log file with timestamps and then reply using accurate times
+* Support compressed log files
+## Apps that work ##
+A list of applications that have been used to view the data.
+So far no apps have been tested to set waypoints.
+### Tested ###
+* inavX [iOS](http://www.inavx.com/)
+* iregatta [iOS](http://itunes.apple.com/gb/app/iregatta/id334632033?mt=8) [android](https://play.google.com/store/apps/details?id=dk.letscreate.aRegatta&hl=en)
+### Untested ###
+* iNMEA [iOS](http://itunes.apple.com/gb/app/inmea/id382867581?mt=8)
--- i2armChairCaptain.py
+++ i2armChairCaptain.py
@@ -1,9 +1,16 @@
 from time import sleep
 import argparse
+import socket
+import signal
+import sys
+from FileThread import *
 
-def processSentance(sentance,sleepLength):
-    print "Sentance is : ", sentance
-    sleep(sleepLength)
+def signal_handler(signal, frame):
+        fileThread.exit()
+        sys.exit(0)
+        
+
+signal.signal(signal.SIGINT, signal_handler)
 
 parser = argparse.ArgumentParser(description="Navigation simulator. Plays back log files over the network")
 parser.add_argument('--rate', type=int,  default=1    ,  nargs='?', help='how long to sleep for between sending each NMEA sentance')
@@ -12,16 +19,40 @@ parser.add_argument( 'infile', nargs='?', type=argparse.FileType('r'), help='Fil
 
 args = parser.parse_args()
 print args
+
+
+
 #logFile = open(,'r')
 sentance = ""
-for line in args.infile:
-    if line == "":
-        exit
-    line = line[0:1]
-    sentance += line
-    if line == "\n":
-        if len(sentance) > 4:
-            processSentance(sentance,args.rate)
-        sentance = ""
+TCP_PORT = 2114
+UDP_PORT = 51423
+
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+udpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+udpSocket.bind(( '' , UDP_PORT))
+
+
+global fileThread
+fileThread = FileThread(args.infile,args.rate)
+fileThread.addSocket(udpSocket)
+fileThread.start()
+
+#ser = serial.Serial('/dev/ttyUSB0', 4800, timeout=1)
+    #x = ser.read()          # read one byte
+    #s = ser.read(10)        # read up to ten bytes (timeout)
+    #line = ser.readline()   # read a '\n' terminated line
+server = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
+server.bind ( ( '', TCP_PORT ) )
+server.listen ( 1 )
+
+while True:
+	conn, addr = server.accept()
+	print ("Connection from", addr)
+	fileThread.addSocket(conn)
+server.close()
+
 
         
